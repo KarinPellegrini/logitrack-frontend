1a2cb59d697f451ad4266a1d0429c8ed332a24e8
KarinPellegrini <105886847+KarinPellegrini@users.noreply.github.com>
2026-04-02 20:29:04 -0300

    Update logitrackApi.js

--- src/api/logitrackApi.js
+++ src/api/logitrackApi.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const logitrackApi = axios.create({
-  // Usamos VITE_API_BASE_URL para coincidir con tu búsqueda anterior en VS Code.
-  // El "||" asegura que si estás en tu PC (donde la variable no existe), use localhost.
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api'
+    baseURL: 'http://localhost:8080/api'
 });
 
 export default logitrackApi;
